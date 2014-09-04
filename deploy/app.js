9edf58cd2587a9510fc7d9303f6f8efb2003b158
Guillermo Martel <martelgames@gmail.com>
2014-09-03 21:41:19 -0600

    ajustando ip de backend

--- deploy/app.js
+++ deploy/app.js
@@ -3,7 +3,13 @@
 
 	var config = {};
 
-	config.ferrisIp = 'music-app-bakcend-test1.appspot.com'; 
+    //set backend base url
+	if ( location.host.indexOf('localhost') != -1 ) {
+		config.ferrisIp = 'localhost:11080';
+	}
+	else {
+		config.ferrisIp = 'music-app-bakcend-test1.appspot.com'; 
+	}
 
 	scope.appConfig = config;
 
